f7a63c25889aa68a8a5efc9ee435d4ac452a1e13
mosab10010 <Mosab.Hasen.abt.js@tuwaiq.edu.sa>
2021-11-09 21:18:04 +0300

    home work done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,130 @@
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.css"
+import {
+  Nav,
+  Navbar,
+  NavDropdown,
+  Form,
+  Container,
+  FormControl,
+  Button,
+  Card,
+  CardGroup,
+  Carousel,
+} from "react-bootstrap"
+
 function App() {
-  return <></>
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">apple iphone</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "100px" }} navbarScroll>
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">Link</Nav.Link>
+              <NavDropdown title="Link" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#" disabled>
+                Link
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <FormControl type="search" placeholder="Search" className="me-2" aria-label="Search" />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <CardGroup>
+        <Card>
+          <Card.Img
+            variant="top"
+            src="https://www.jarir.com/cdn-cgi/image/fit=contain,width=400,height=400/https://www.jarir.com/media//catalog/product/5/6/569296.jpg?1"
+          />
+          <Card.Body>
+            <Card.Title>iphone 13</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural lead-in to additional content. This content
+              is a little bit longer.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img
+            variant="top"
+            src="https://www.jarir.com/cdn-cgi/image/fit=contain,width=400,height=400/https://www.jarir.com/media//catalog/product/5/6/569296.jpg?1"
+          />
+          <Card.Body>
+            <Card.Title>iphone 13</Card.Title>
+            <Card.Text>This card has supporting text below as a natural lead-in to additional content. </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img
+            variant="top"
+            src="https://www.jarir.com/cdn-cgi/image/fit=contain,width=400,height=400/https://www.jarir.com/media//catalog/product/5/6/569296.jpg?1"
+          />
+          <Card.Body>
+            <Card.Title>iphone 13</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural lead-in to additional content. This card has
+              even longer content than the first to show that equal height action.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+      </CardGroup>
+      <Carousel>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            src="https://media.ldlc.com/r1600/ld/products/00/05/70/84/LD0005708491_1_0005708496_0005708501_0005900603.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <img
+            className="d-block w-100"
+            src="https://support.apple.com/library/APPLE/APPLECARE_ALLGEOS/SP759/Imac21inch4k2017.png"
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://alephksa.com/wp-content/uploads/MEAR_iMac_24-in_Blue_2-port_PDP_Image_Position-1-1-350x350.jpg"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </>
+  )
 }
 
 export default App
