aae4ee9792d7fdd4797a09f4cc9b8e20d883e14a
Mohammmed1101 <Mohammed.Khaled.abt.js@tuwaiq.edu.sa>
2021-11-10 01:05:34 +0300

    A1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Navbar from "./component/Navbar"
+import Card from "./component/Card"
+import Carousel from "./component/Carousel"
+
 function App() {
-  return <></>
+  return (
+    <>
+      <Navbar />
+      <Card />
+      <Carousel />
+    </>
+  )
 }
 
 export default App
