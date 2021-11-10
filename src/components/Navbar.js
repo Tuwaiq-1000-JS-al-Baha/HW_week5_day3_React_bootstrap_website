7939fbc69b0fa4be6dc653ed7d210a22158fd9a4
X702-2025 <mohmmad201048@gmail.com>
2021-11-10 09:38:58 +0300

    added logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,18 @@ import {Navbar , Container, Nav} from 'react-bootstrap'
 function NavigationBar() {
     return ( 
         <>
-     <Navbar bg="dark" variant="dark">
+     <Navbar bg="black" variant="dark">
     <Container>
-    <Navbar.Brand href="#home">Nature Website</Navbar.Brand>
+    <Navbar.Brand href="#home">
+    <img
+        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F4a%2Fdd%2Ffe%2F4addfec42b8469dbc19a7f594dab6c3c.jpg&f=1&nofb=1"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+        />{' '}
+        Nature Website
+      
+      </Navbar.Brand>
     <Nav className="me-auto">
       <Nav.Link href="#Home">Home</Nav.Link>
       <Nav.Link href="#Gallery">Gallery</Nav.Link>
