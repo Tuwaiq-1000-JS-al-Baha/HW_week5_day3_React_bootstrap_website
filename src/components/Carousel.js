05e2ef277c7c4593e8153517d9926b09f5198c82
Adnen Ben Amor <adnene.benamor@tuwaiq.edu.sa>
2021-11-10 11:06:27 +0300

    done hw

---
+++ src/components/Carousel.js
@@ -0,0 +1,19 @@
+import { Carousel } from "react-bootstrap"
+
+function CarouselGroup(props) {
+  return (
+    <Carousel className="d-block w-50 mx-auto mt-5">
+      {props.carouselItems.map(carouselItem => (
+        <Carousel.Item>
+          <img height="500px" src={carouselItem.image} alt="First slide" />
+          <Carousel.Caption>
+            <h3>{carouselItem.title}</h3>
+            <p>{carouselItem.body}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  )
+}
+
+export default CarouselGroup
