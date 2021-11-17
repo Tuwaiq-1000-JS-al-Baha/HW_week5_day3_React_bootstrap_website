17a4ca963ed68e1f32c0186a69991df1d313ec4c
Najmah24 <92245799+Najmah24@users.noreply.github.com>
2021-11-17 10:11:05 +0300

    doon

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import Navbaar from "./components/Navbaar"
+import Cardd from "./components/Cardd"
+import Carouselse from "./components/Carouselse"
+
+function App(props) {
+  return (
+    <>
+      <Navbaar />
+      <Cardd />
+      <Carouselse />
+    </>
+  )
+}
+
+export default App
